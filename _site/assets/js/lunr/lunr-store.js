93f208cc146481210300d537313a6a7d150cd7b4
Yongho Son <sssonly87@gmail.com>
2021-03-26 09:55:13 +0900

    update

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -5,10 +5,10 @@ var store = [{
         "url": "/blog/2021/02/14/expo-eject-ios-appdelegate-issue.html",
         "teaser": null
       },{
-        "title": "경제의 기본적 이해 #1",
+        "title": "레이달리오 경제의 기본 #1",
         "excerpt":"출처: Big Debt Crises by Ray Dalio를 번역+의역한 내용입니다. 전체 세계/국내 경제의 큰 흐름을 읽을 수 있는 눈을 키우기 위해 필요한 이해와 지식에 대한 설명. 크게 다음과 같은 순서로 설명 신용과 부채란 무엇인가 전형적인 큰 부채 사이클 디플레이션 부채 사이클을 구성하는 단계들 인플레이션적인 침체와 통화위기 인플레이션 부채 사이클을 구성하는 단계들...","categories": [],
         "tags": [],
-        "url": "/blog/2021/02/17/%EA%B2%BD%EC%A0%9C-%EA%B8%B0%EB%B3%B8%EC%A0%81-%EC%9D%B4%ED%95%B4-1.html",
+        "url": "/blog/2021/02/17/%EB%A0%88%EC%9D%B4%EB%8B%AC%EB%A6%AC%EC%98%A4-%EA%B2%BD%EC%A0%9C%EC%9D%98-%EA%B8%B0%EB%B3%B8-1.html",
         "teaser": null
       },{
         "title": "찰리멍거의 심리학 #1",
@@ -17,52 +17,52 @@ var store = [{
         "url": "/blog/2021/02/26/%EC%B0%B0%EB%A6%AC%EB%A9%8D%EA%B1%B0%EC%9D%98-%EC%8B%AC%EB%A6%AC%ED%95%99-1.html",
         "teaser": null
       },{
-        "title": "경제의 기본적 이해 #2",
+        "title": "레이달리오 경제의 기본 #2",
         "excerpt":"출처: Big Debt Crises by Ray Dalio를 번역+의역한 내용입니다. 전체 세계/국내 경제의 큰 흐름을 읽을 수 있는 눈을 키우기 위해 필요한 이해와 지식에 대한 설명. 크게 다음과 같은 순서로 설명 신용과 부채란 무엇인가 전형적인 큰 부채 사이클 디플레이션 부채 사이클을 구성하는 단계들 인플레이션적인 침체와 통화위기 인플레이션 부채 사이클을 구성하는 단계들...","categories": [],
         "tags": [],
-        "url": "/blog/2021/03/05/%EA%B2%BD%EC%A0%9C-%EA%B8%B0%EB%B3%B8%EC%A0%81-%EC%9D%B4%ED%95%B4-2.html",
+        "url": "/blog/2021/03/05/%EB%A0%88%EC%9D%B4%EB%8B%AC%EB%A6%AC%EC%98%A4-%EA%B2%BD%EC%A0%9C%EC%9D%98-%EA%B8%B0%EB%B3%B8-2.html",
         "teaser": null
       },{
         "title": "성장을 위한 지식 #1 - 금융시장의 현재 흐름",
         "excerpt":"경제 2021.03.09 시장의 중요한 흐름 시장의 분위기는 코로나 백신 접종 시작과 더불어 코로나 시대가 곧 끝날 것이라는 기대가 증가하는 중. 미국은 추가적인 2000조 부양책을 대통령 서명만 남겨놓고 있는 상태. 10년물 미국 국채 수익률의 가파른 상승 (=국채 가격의 하락). 2021년 초에 1% 이하였던 수익률이 현재 1.6%까지 상승. 기술주/언택트주(아마존, 애플, 테슬라, 줌)...","categories": [],
         "tags": [],
-        "url": "/blog/2021/03/09/%EC%84%B1%EC%9E%A5%EC%9D%84-%EC%9C%84%ED%95%9C-%EC%A7%80%EC%8B%9D-1.html",
+        "url": "/blog/2021/03/09/KG-1-%EA%B8%88%EC%9C%B5%EC%8B%9C%EC%9E%A5%EC%9D%98-%ED%98%84%EC%9E%AC-%ED%9D%90%EB%A6%84.html",
         "teaser": null
       },{
         "title": "성장을 위한 지식 #2 - 사업을 구성하는 5가지 요소",
         "excerpt":"Business &amp; Startup 사업을 구성하는 5가지 요소 가치생산: 사람들이 필요로 하거나 원하는 것을 만드는 것 마케팅: 만든 것을 알리는 것 판매: 잠재고객을 실제고객으로 만드는 것 가치전달: 약속한 것을 전달하고 실제로 사람들이 만족하는지 확인하는 것 재무: 이것을 계속할 수 있을만큼 충분한 돈을 충당하는 것 예1) 구글 가치생산: 필요한 정보를 인터넷에서 쉽고...","categories": [],
         "tags": [],
-        "url": "/blog/2021/03/10/%EC%84%B1%EC%9E%A5%EC%9D%84-%EC%9C%84%ED%95%9C-%EC%A7%80%EC%8B%9D-2.html",
+        "url": "/blog/2021/03/10/KG-2-%EC%82%AC%EC%97%85%EC%9D%84-%EA%B5%AC%EC%84%B1%ED%95%98%EB%8A%94-5%EA%B0%80%EC%A7%80-%EC%9A%94%EC%86%8C.html",
         "teaser": null
       },{
         "title": "성장을 위한 지식 #3 - 인간의 기본적인 욕구",
         "excerpt":"Business &amp; Startup 인간의 기본적인 욕구 &amp; 행동의 동기와 관련한 유명한 내용들 메슬로의 계층적인 욕구 (5대 욕구) Physiology (생리학 = 생존) Safety (안전) Beloging/love (소속감 &amp; 사랑) Esteem (존중) Self-actualization (자아실현) 클레이튼의 ERG 이론 (3대 욕구) Existence (생존) Relatedness (관계) Growth (성장) 폴 로렌스 &amp; 니친 노리아의 저서 Driven (5대...","categories": [],
         "tags": [],
-        "url": "/blog/2021/03/11/%EC%84%B1%EC%9E%A5%EC%9D%84-%EC%9C%84%ED%95%9C-%EC%A7%80%EC%8B%9D-3.html",
+        "url": "/blog/2021/03/11/KG-3-%EC%9D%B8%EA%B0%84%EC%9D%98-%EA%B8%B0%EB%B3%B8%EC%A0%81%EC%9D%B8-%EC%9A%95%EA%B5%AC.html",
         "teaser": null
       },{
         "title": "성장을 위한 지식 #4 - 사회적 지위를 제품/서비스에 반영하는 것, 시장을 평가하는 10가지 방법",
         "excerpt":"Business &amp; Startup 사회적 지위(Social status)를 제품/서비스에 반영하는 것 사람은 사회적인 동물이기 때문에 본인이 속한 사회와 집단안에서의 상대적인 지위 및 서열에 민감하게 반응하는 특성이 있다. 이러한 특성이 존재하는 이유는 진화적인 관점에서 이해할 수 있는데, 과거 음식과 안전한 거처가 풍족하지 않은 시대에서는 더 높은 지위를 가질 수록 이런 생존에 중요한 자원들을...","categories": [],
         "tags": [],
-        "url": "/blog/2021/03/12/%EC%84%B1%EC%9E%A5%EC%9D%84-%EC%9C%84%ED%95%9C-%EC%A7%80%EC%8B%9D-4.html",
+        "url": "/blog/2021/03/12/KG-4-%EC%82%AC%ED%9A%8C%EC%A0%81-%EC%A7%80%EC%9C%84%EB%A5%BC-%EC%A0%9C%ED%92%88%EC%97%90-%EB%B0%98%EC%98%81%ED%95%98%EB%8A%94%EA%B2%83.html",
         "teaser": null
       },{
-        "title": "컴퓨터 공학 및 프로그래밍 지식 #1",
-        "excerpt":"출처: Computer Systems - A Programmer’s Perspective 2nd edition by Bryant &amp; O’Hallaron Data alignment 프로세서가 메모리에서 값을 가져올 때 특정 배수단위로 주소를 기반으로 가져 오기 때문에 성능 최적화를 위해서는 메모리에 저장되는 각 타입(short, int, double)의 주소도 메모리상에 배수형태(2, 4, 8)로 저장되어야 함. 이를 위해 struct type과 같은 경우 내부에...","categories": [],
+        "title": "컴퓨터 공학 및 프로그래밍 #1 - Data alignment, 포인터, Buffer Overflow",
+        "excerpt":"Data alignment 프로세서가 메모리에서 값을 가져올 때 특정 배수단위로 주소를 기반으로 가져 오기 때문에 성능 최적화를 위해서는 메모리에 저장되는 각 타입(short, int, double)의 주소도 메모리상에 배수형태(2, 4, 8)로 저장되어야 함. 이를 위해 struct type과 같은 경우 내부에 혹은 마지막에 추가적인 바이트를 넣어서 struct 내부의 값 혹은 element를 struct로 갖는 array의...","categories": [],
         "tags": [],
-        "url": "/blog/2021/03/13/%EC%BB%B4%ED%93%A8%ED%84%B0-%EA%B3%B5%ED%95%99-%EB%B0%8F-%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%B0%8D-%EC%A7%80%EC%8B%9D-1.html",
+        "url": "/blog/2021/03/13/CS-1-%EC%BB%B4%ED%93%A8%ED%84%B0-%EA%B3%B5%ED%95%99-%EB%B0%8F-%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%B0%8D-%EC%A7%80%EC%8B%9D-1.html",
         "teaser": null
       },{
         "title": "성장을 위한 지식 #5 - 권위에 순응하는 심리학, 경쟁의 숨겨진 이익, 용병의 법칙",
         "excerpt":"심리학 직업 타이틀(권위)에 순응하는 경향 사람들은 권위 및 사회적으로 지위가 높은 타이틀을 가지는 사람들의 말에 순응하는 경향이 존재한다. 자기보다 사회적 서열이 높거나 높은 자리에 있는 사람들(선생님, 교수, 의사, 검사, 정치인, 대통령, 고위공무원 등)이 하는 말을 스스로 생각해서 합리적인지 분석하지 않고 있는 그대로 따르는 현상이다. 대부분의 경우에는 이들의 이야기는 더 객관적이고...","categories": [],
         "tags": [],
-        "url": "/blog/2021/03/13/%EC%84%B1%EC%9E%A5%EC%9D%84-%EC%9C%84%ED%95%9C-%EC%A7%80%EC%8B%9D-5.html",
+        "url": "/blog/2021/03/13/KG-5-%EA%B6%8C%EC%9C%84%EC%97%90-%EC%88%9C%EC%9D%91%ED%95%98%EB%8A%94-%EC%8B%AC%EB%A6%AC%ED%95%99-%EA%B2%BD%EC%9F%81%EC%9D%98-%EC%88%A8%EA%B2%A8%EC%A7%84-%EC%9D%B4%EC%9D%B5-%EC%9A%A9%EB%B3%91%EC%9D%98-%EB%B2%95%EC%B9%99.html",
         "teaser": null
       },{
         "title": "성장을 위한 지식 #6 - 12가지의 비즈니스 형태",
         "excerpt":"Business &amp; Startup 12가지 일반적인 비즈니스 형태 우리들의 사업이 성공하기 위해서는 사람들에게 가치를 주어야 하고 이것은 사람들이 돈을 지불할 수 있도록 어떠한 형태를 갖고 있어야 한다. 다음은 12가지의 일반적인 가치의 형태와 이것들에 단순화된 개념이다. 제품: 유형의 물건 (물리적일 수도 디지털 형태일 수도 있음)을 만들어서 파는것. 만드는 비용보다 더 높은 가격에...","categories": [],
         "tags": [],
-        "url": "/blog/2021/03/14/%EC%84%B1%EC%9E%A5%EC%9D%84-%EC%9C%84%ED%95%9C-%EC%A7%80%EC%8B%9D-6.html",
+        "url": "/blog/2021/03/14/KG-6-12%EA%B0%80%EC%A7%80-%EB%B9%84%EC%A6%88%EB%8B%88%EC%8A%A4-%ED%98%95%ED%83%9C.html",
         "teaser": null
       },{
         "title": "성장을 위한 지식 #7 - 비트코인과 암호화폐",
@@ -81,5 +81,11 @@ var store = [{
         "excerpt":"경제적 가치 고객이 우리에게 구매를 하는 매순간은 고객들은 그 시점에 그 돈으로 그들이 살 수 있는 다른 것보다 우리가 제공하는 것을 더 가치있다고 결정하는 것을 의미한다. 제품/서비스를 향상시키고 개발하면서 제일 중요한 것 중에 하나는 우리의 잠재고객들이 그들의 지갑에 있는 돈의 구매력보다 다른 어떤 것을 더 가치있게 생각하는지를 파악하는 일이다. 모든...","categories": [],
         "tags": [],
         "url": "/blog/2021/03/23/KG-9-%EA%B2%BD%EC%A0%9C%EC%A0%81-%EA%B0%80%EC%B9%98%EC%99%80-%EC%83%81%EB%8C%80-%EC%A4%91%EC%9A%94%EB%8F%84-%ED%85%8C%EC%8A%A4%ED%8A%B8.html",
+        "teaser": null
+      },{
+        "title": "컴퓨터 공학 및 프로그래밍 #2- 그래프 데이터 모델",
+        "excerpt":"그래프 데이터 모델 그래프 데이터 모델은 many-to-many관계가 데이터 간에 많이 존재할때 사용하기에 더 유용한 모델이다. 모든 그래프는 크게 2가지로 구성되는데 하나는 꼭지점이고 다른 하나는 모서리다. 소셜네트워크 어플리케이션에서 꼭지점은 사람을 표현하고 모서리는 사람들간의 팔로잉 팔로워를 표현하게 된다. 페이스북이 사용하는 그래프 데이터 모델의 경우 꼭지점은 사람만을 의미하는 homogeneous타입이 아니고 사람, 위치, 체크인...","categories": [],
+        "tags": [],
+        "url": "/blog/2021/03/24/CS-2-%EA%B7%B8%EB%9E%98%ED%94%84-%EB%8D%B0%EC%9D%B4%ED%84%B0-%EB%AA%A8%EB%8D%B8.html",
         "teaser": null
       }]
